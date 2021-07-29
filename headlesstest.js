28a39c53b94723dc5346dcb2e4cea6f3ef47a6f5
Ainesh Sootha <aineshsootha@gmail.com>
2021-07-29 16:01:18 -0400

    updated code to match standards

--- headlesstest.js
+++ headlesstest.js
@@ -6,53 +6,24 @@ var fs = require('fs');
 var path = require('path');
 global.appRoot = path.resolve(__dirname);
 
-var footerCalls = 0;
 
-function footerTFirstPage() {
-    return `
-    <style>
-        footer:first-of-type{
-            color: aqua;
-        }
-    </style>
-    <footer class="footerPuppeteer" style="border-top: solid 1px #bbb; width: 100%; font-size: 9px;
-    padding: 5px 5px 0; color: #bbb; position: relative;">
-        <div style="position: absolute; left: 5px; top: 5px;">This is a different footer for the First Page</div>
-        <div style="position: absolute; right: 5px; top: 5px;">First Page</div>
-    </footer>
-    `
+function footerT(page) {
+    var footer = `<div class="footerPuppeteer" style="border-top: solid 1px #bbb; width: 100%; font-size: 9px; padding: 5px 5px 0; color: #bbb; position: relative;">`
+    if (page === 'first') {
+        footer += `<div style="position: absolute; left: 5px; top: 5px;">This is a different footer for the First Page</div>`;
+        footer += `<div style="position: absolute; right: 5px; top: 5px;">First Page</div>`;
+    } else if (page === 'last') {
+        footer += `<div style="position: absolute; left: 5px; top: 5px;">This is a different footer for the Last Page</div>`
+        footer += `<div style="position: absolute; right: 5px; top: 5px;">Page <span class="pageNumber"></span> of <span class="totalPages"></span></div>`
+    } else {
+        footer += `<div style="position: absolute; left: 5px; top: 5px;"><span class="date"></span></div>`;
+        footer += `<div style="position: absolute; right: 5px; top: 5px;">Page <span class="pageNumber"></span> of <span class="totalPages"></span></div>`
+    }
+    footer += `</div>`;
+    return footer
 }
 
-function footerTLastPage() {
-    return `
-    <style>
-        footer:first-of-type{
-            color: aqua;
-        }
-    </style>
-    <footer class="footerPuppeteer" style="border-top: solid 1px #bbb; width: 100%; font-size: 9px;
-    padding: 5px 5px 0; color: #bbb; position: relative;">
-        <div style="position: absolute; left: 5px; top: 5px;">This is a different footer for the Last Page</div>
-        <div style="position: absolute; right: 5px; top: 5px;">Page <span class="pageNumber"></span> of <span class="totalPages"></span></div>
-    </footer>
-    `
-}
 
-function footerT() {
-    footerCalls++;
-    return `
-    <style>
-        footer:first-of-type{
-            color: aqua;
-        }
-    </style>
-    <footer class="footerPuppeteer" style="border-top: solid 1px #bbb; width: 100%; font-size: 9px;
-    padding: 5px 5px 0; color: #bbb; position: relative;">
-        <div style="position: absolute; left: 5px; top: 5px;"><span class="date"></span></div>
-        <div style="position: absolute; right: 5px; top: 5px;">Page <span class="pageNumber"></span> of <span class="totalPages"></span></div>
-    </footer>
-    `
-}
 
 function base64Encode(file) {
     return fs.readFileSync(file, { encoding: 'base64' });
@@ -70,7 +41,6 @@ function logo() {
     console.time('newPage');
     const page = await browser.newPage();
     console.timeEnd('newPage');
-    //const pageURL = 'file://${path.join(__dirname, example.html'
     console.time('goto');
     await page.goto(`file:${path.join(__dirname, 'example.html')}`); //await page.goto(pageURL);
     console.timeEnd('goto');
@@ -79,41 +49,47 @@ function logo() {
     await watchDog;
     console.timeEnd('waitForFun');
     console.time('firstPDF');
+
     const firstPage = await page.pdf({
-        //path: 'portrait3.pdf',
         displayHeaderFooter: true,
         headerTemplate: logo(),
-        footerTemplate: footerTFirstPage(),
+        footerTemplate: footerT("first"),
         pageRanges: '1',
         margin: { top: "50px", bottom: "100px" },
     });
     console.timeEnd('firstPDF');
+
     console.time('secondPDF');
     await page.pdf({
         path: 'portrait3.pdf',
         displayHeaderFooter: true,
         headerTemplate: logo(),
-        footerTemplate: footerT(),
+        footerTemplate: footerT("main"),
         pageRanges: '2-',
         margin: { top: "50px", bottom: "100px" },
     });
     console.timeEnd('secondPDF');
-    console.time('PDF Merges');
+
     const mainPDFBuffer = fs.readFileSync('portrait3.pdf');
     const pdfForPageNums = await pdf(mainPDFBuffer);
     let numPages = pdfForPageNums.numpages;
+
+    console.time('thirdPDF');
     const lastPage = await page.pdf({
         displayHeaderFooter: true,
         headerTemplate: logo(),
-        footerTemplate: footerTLastPage(),
+        footerTemplate: footerT("last"),
         pageRanges: `${numPages + 1}`,
         margin: { top: "50px", bottom: "100px" },
     });
-    const firstPageDoc = await PDFDocument.load(firstPage);
-    const lastPageDoc = await PDFDocument.load(lastPage);
-    const mainPDF = await PDFDocument.load(mainPDFBuffer);
-    const copiedFirstPage = await mainPDF.copyPages(firstPageDoc, [0]);
-    const copiedLastPage = await mainPDF.copyPages(lastPageDoc, [0]);
+    console.timeEnd('thirdPDF');
+
+    console.time('PDF Merges');
+    let firstPageDoc = await PDFDocument.load(firstPage);
+    let lastPageDoc = await PDFDocument.load(lastPage);
+    let mainPDF = await PDFDocument.load(mainPDFBuffer);
+    let copiedFirstPage = await mainPDF.copyPages(firstPageDoc, [0]);
+    let copiedLastPage = await mainPDF.copyPages(lastPageDoc, [0]);
     mainPDF.removePage(numPages - 1); //Delete last page with wrong footer
     mainPDF.insertPage(0, copiedFirstPage[0]); //Add first page
     mainPDF.addPage(copiedLastPage[0]); //Add last page with correct footer
