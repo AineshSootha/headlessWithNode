6d4e0650c4feaa5c65d4445c216b9fe5026cf072
Ainesh Sootha <aineshsootha@gmail.com>
2021-07-30 14:43:11 -0400

    removed fixed outputPath

--- headlesstest.js
+++ headlesstest.js
@@ -6,7 +6,6 @@ var fs = require('fs');
 var path = require('path');
 global.appRoot = path.resolve(__dirname);
 
-
 function footerT(page) {
     var footer = `<div class="footerPuppeteer" style="border-top: solid 1px #bbb; width: 100%; font-size: 9px; padding: 5px 5px 0; color: #bbb; position: relative;">`
     if (page === 'first') {
@@ -23,31 +22,54 @@ function footerT(page) {
     return footer
 }
 
-
 function emptyHeader() {
     return '<div></div>';
 }
 
-
-
 (async() => {
     const pageURL = process.argv[2];
+    const outputPath = process.argv[3];
+
     console.log(pageURL);
     console.time('start');
+
     console.time('launch');
     const browser = await puppeteer.launch({});
     console.timeEnd('launch');
+
     console.time('newPage');
     const page = await browser.newPage();
     console.timeEnd('newPage');
+
+
+
+
+    let isPrintReady = false;
+    page.on('console', msg => {
+        console.log(msg.text());
+        if (msg.text() === 'print-ready') {
+            console.log("Print Ready Now. Proceeding with pdf generation.");
+            isPrintReady = true;
+        }
+    });
+    page.exposeFunction("checkPrintReady", function() {
+        return isPrintReady;
+    });
+
+
     console.time('goto');
     await page.goto(pageURL);
-    console.log(`file:${path.join(__dirname, 'example.html')}`); //
+    //console.log(`file:${path.join(__dirname, 'example.html')}`); //
     console.timeEnd('goto');
+
     console.time('waitForFun');
-    const watchDog = page.waitForFunction('done.length === 4');
-    await watchDog;
+    console.log("Waiting for print-ready message");
+    await page.waitForFunction(() => {
+        return checkPrintReady();
+    })
+    console.log("print-ready message received");
     console.timeEnd('waitForFun');
+
     console.time('firstPDF');
     const firstPage = await page.pdf({
         displayHeaderFooter: true,
@@ -60,7 +82,7 @@ function emptyHeader() {
 
     console.time('secondPDF');
     await page.pdf({
-        path: 'portrait3.pdf',
+        path: outputPath,
         displayHeaderFooter: true,
         headerTemplate: emptyHeader(),
         footerTemplate: footerT("main"),
@@ -69,7 +91,7 @@ function emptyHeader() {
     });
     console.timeEnd('secondPDF');
 
-    const mainPDFBuffer = fs.readFileSync('portrait3.pdf');
+    const mainPDFBuffer = fs.readFileSync(outputPath);
     const pdfForPageNums = await pdf(mainPDFBuffer);
     let numPages = pdfForPageNums.numpages;
 
@@ -92,7 +114,7 @@ function emptyHeader() {
     mainPDF.removePage(numPages - 1); //Delete last page with wrong footer
     mainPDF.insertPage(0, copiedFirstPage[0]); //Add first page
     mainPDF.addPage(copiedLastPage[0]); //Add last page with correct footer
-    fs.writeFileSync('portrait3.pdf', await mainPDF.save());
+    fs.writeFileSync(outputPath, await mainPDF.save());
     console.timeEnd('PDF Merges');
     await browser.close();
     console.timeEnd('start');
